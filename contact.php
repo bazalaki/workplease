897c4aa7d9446cc151c91d20f0190d95c1e8e439
bazalaki <40613708+bazalaki@users.noreply.github.com>
2018-12-11 15:15:29 -0600

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,23 @@
+<?php
+// form handler
+if($_POST && isset($_POST['sendfeedback'], $_POST['name'], $_POST['email'], $_POST['subject'], $_POST['message'])) {
+
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $subject = $_POST['subject'];
+  $message = $_POST['message'];
+
+  if(!$name) {
+    $errorMsg = "Please enter your Name";
+  } elseif(!$email || !preg_match("/^\S+@\S+$/", $email)) {
+    $errorMsg = "Please enter a valid Email address";
+  } elseif(!$message) {
+    $errorMsg = "Please enter your comment in the Message box";
+  } else {
+    // redirect
+    echo "Thank You!" . " -" . "<a href=index.html";
+    
+  }
+
+}
+?>
